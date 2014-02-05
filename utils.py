977b8f962575691ad371f9fbbdc38720fc4f1098
Dmitry Mekhantev <mekhantev@live.com>
2014-02-05 20:43:24 +0400

    Rename open_in_browser response_text to text

--- utils.py
+++ utils.py
@@ -2,8 +2,8 @@ from datetime import datetime
 import webbrowser
 
 
-def open_in_browser(response_text):
+def open_in_browser(text):
     temp_file = 'temp_{}.html'.format(datetime.now().timestamp())
     with open(temp_file, 'w') as html:
-        html.write(response_text)
+        html.write(text)
     webbrowser.open_new_tab(temp_file)
