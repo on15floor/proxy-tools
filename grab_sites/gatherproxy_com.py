c063a49849c69374ca5cac4961761f56f99aacf9
Aleksandr Anisimov <on15floor@gmail.com>
2014-02-06 21:38:04 +0400

    Change return type from str to list of grab_proxies function

--- grab_sites/gatherproxy_com.py
+++ grab_sites/gatherproxy_com.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def grab():
+def grab_proxies():
+    print('[i]Grab gatherproxy.com:')
     response = requests.get("http://gatherproxy.com/ru/subscribe/login")
     session_id_key = 'ASP.NET_SessionId'
     session_id_value = response.cookies[session_id_key]
@@ -29,7 +30,7 @@ def grab():
     if 'Logout' not in response.text:
         raise Exception('[e]Can\'t Login')
     if 'LOGIN' not in response.text:
-        print('[e]Login success, Start grab')
+        print('[+]Login success, Start grab')
 
     html = BeautifulSoup(response.text)
     link = html.select('a[href^="/proxylist/downloadproxylist/?sid="]')[0]['href']
@@ -41,11 +42,10 @@ def grab():
     response = requests.post("http://gatherproxy.com" + link,
                              data=data, headers=headers, cookies=cookies, allow_redirects=False)
 
-    return response.text
+    return [proxy for proxy in response.text.split('\r\n')]
 
 
 def ask_google(question):
-    print(question)
     g_search = urllib.parse.quote_plus(question.text)
     g_search = g_search.replace('multiplied', '*')
 
@@ -53,8 +53,5 @@ def ask_google(question):
     html = BeautifulSoup(response.text)
     answer = html.find("h2", {"class": "r"}).text.split('=')[1].strip()
 
-    print('[i]Google:' + answer)
+    print('[i]Google:' + g_search + '=' + answer)
     return answer
-
-
-grab()
