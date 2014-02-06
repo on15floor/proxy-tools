f79ee5a692b2943eaa4fe060bfb26046c4bc63f6
Aleksandr Anisimov <on15floor@gmail.com>
2014-02-06 21:27:58 +0400

    Refactor hideme_ru

--- grab_sites/hideme_ru.py
+++ grab_sites/hideme_ru.py
@@ -2,32 +2,24 @@ import random
 import requests
 
 
-def grab():
+def grab_proxies():
     print('[i]Grab hideme.ru:')
     for i in range(1, 4):
         key = random.randint(100000, 999999)
-        try:
-            result = check(key)
-            # print(key)
-            return result
-            break
-        except:
-            pass
 
+        data = {
+            'c': key
+        }
+        headers = {
+            'Content_Type': 'application/x-www-form-urlencoded'
+        }
+        response = requests.post("http://hideme.ru/login",
+                                 data=data, headers=headers, allow_redirects=False)
 
-def check(key):
-    data = {
-        'c': key
-    }
-    headers = {
-        'Content_Type': 'application/x-www-form-urlencoded'
-    }
-    response = requests.post("http://hideme.ru/login",
-                             data=data, headers=headers, allow_redirects=False)
-
-    if response.status_code == 302:
-        proxies = requests.get("http://hideme.ru/api/proxylist.txt?out=plain&lang=ru")
-        print('[+]Hideme.ru key: ' + str(key))
-        return proxies.content.decode().strip()
-    else:
-        raise RuntimeError()
+        if response.status_code == 302:
+            proxies = requests.get("http://hideme.ru/api/proxylist.txt?out=plain&lang=ru")
+            print('[+]Hideme.ru key: ' + str(key))
+            return [proxy for proxy in proxies.content.decode().split('\r\n')]
+        else:
+            if i == 3:
+                print('[-]Hideme.ru: Key not found')
