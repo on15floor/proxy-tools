5ef5d297a7a43dfd44cb5d782012e94c790896a7
Aleksandr Anisimov <on15floor@gmail.com>
2014-02-08 21:47:02 +0400

    Remove unnecessary print function

--- grab_sites/hideme_ru.py
+++ grab_sites/hideme_ru.py
@@ -3,7 +3,6 @@ import requests
 
 
 def grab_proxies():
-    print('[i]Grab hideme.ru:')
     for i in range(1, 4):
         key = random.randint(100000, 999999)
 
@@ -18,8 +17,8 @@ def grab_proxies():
 
         if response.status_code == 302:
             proxies = requests.get("http://hideme.ru/api/proxylist.txt?out=plain&lang=ru")
-            print('[+]Hideme.ru key: ' + str(key))
+            print('[+]->Hideme.ru key: ' + str(key))
             return [proxy for proxy in proxies.content.decode().split('\r\n')]
         else:
             if i == 3:
-                print('[-]Hideme.ru: Key not found')
+                print('[-]->Hideme.ru: Key not found')
