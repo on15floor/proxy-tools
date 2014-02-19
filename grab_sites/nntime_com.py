342b935cf186b287fbfce439c8a32441b8c54e9f
Aleksandr Anisimov <on15floor@gmail.com>
2014-02-19 20:09:02 +0400

    Refactor therealist_ru

--- grab_sites/nntime_com.py
+++ grab_sites/nntime_com.py
@@ -2,22 +2,54 @@ from bs4 import BeautifulSoup
 import requests
 
 
+def get_delimiter(delimiter_trash):
+    delimiter_list = []
+    for i in range(0, delimiter_trash.__len__()):
+        delimiter_list += [delimiter_trash[i][-6:-4]]
+
+    frequency, delimiter = 0, 0
+    for d in delimiter_list:
+        c = delimiter_list.count(d)
+        if c > frequency:
+            frequency, delimiter = c, d
+
+    return delimiter
+
+
 def grab_page(num_page):
+    proxies = []
+    proxy_ip = []
+    proxy_port = []
     page = str(num_page).zfill(2)
     response = requests.get("http://nntime.com/proxy-updated-{}.htm".format(page))
-    html = BeautifulSoup(response.text)
-    tag_proxies = html.findAll("tr", {"class": "odd"})
 
-    return
+    html = BeautifulSoup(response.text)
+    tag_proxy_ip_odd = html.findAll("tr", {"class": "odd"})
+    tag_proxy_ip_even = html.findAll("tr", {"class": "even"})
+    tag_proxy_port = html.findAll("input", {"type": "checkbox"})
+    proxy_amount = tag_proxy_port.__len__()
+    o, e = 0, 0
+    for i in range(0, proxy_amount):
+        if i % 2 == 0:
+            proxy_ip += [tag_proxy_ip_odd[o].text.split('document')[0]]
+            o += 1
+            proxy_port += [tag_proxy_port[i].attrs['value']]
+        else:
+            proxy_ip += [tag_proxy_ip_even[e].text.split('document')[0]]
+            e += 1
+            proxy_port += [tag_proxy_port[i].attrs['value']]
+
+    delimiter = get_delimiter(proxy_port)
+    for i in range(0, proxy_amount):
+        proxies += ['{}:{}'.format(proxy_ip[i], proxy_port[i].split(delimiter)[1])]
+
+    return proxies
 
 
 def grab_proxies():
     proxies = []
-
-    for i in range(1, 2):
+    for i in range(1, 30):
         print('[i]->Nntime.com page: {}'.format(i))
         proxies += grab_page(i)
 
     return proxies
-
-print(grab_proxies())
