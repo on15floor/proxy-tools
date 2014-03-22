a66b9e6a99af408fa5cecd72c8a6a4db852ebe1e
Aleksandr Anisimov <on15floor@gmail.com>
2014-03-22 23:58:41 +0400

    Delete unnecessary good_proxies value

--- proxy_check.py
+++ proxy_check.py
@@ -6,14 +6,12 @@ proxy_type = 'http'
 proxy_manager = ProxyManager(proxy_type)
 
 l = []
-good_proxies = 0
 pb = ProgressBar()
 pb.total = len(proxies)
 
 for proxy in proxy_manager.check_anonymity_parallel(proxies, 50):
-    good_proxies += 1
     pb.current = proxies.index(proxy)
-    pb.good = good_proxies
+    pb.good += 1
     pb.draw()
     l.append(proxy)
 
